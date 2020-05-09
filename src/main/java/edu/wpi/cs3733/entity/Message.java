dd170216761642f4db7d35e59158c33c5ade2e2b
npelletier2 <npelletier@wpi.edu>
2020-05-09 15:20:43 -0400

    Added gitignore

--- src/main/java/edu/wpi/cs3733/entity/Message.java
+++ src/main/java/edu/wpi/cs3733/entity/Message.java
@@ -4,13 +4,16 @@ package edu.wpi.cs3733.entity;
  * Message class to hold clear text; you must implement the rest of the class
  */
 public class Message implements Observable {
+	private String text;
 
 	public void setText(String text){
-
+		int max = 140;
+		if(text.length() < max) max = text.length();
+		this.text = text.substring(0, max);
 	}
 
 	public String getText(){
-		return null;
+		return text;
 	}
 
 	@Override
