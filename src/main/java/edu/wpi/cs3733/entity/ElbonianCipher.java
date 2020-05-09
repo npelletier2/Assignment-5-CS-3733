439d7edd8adbfd7280cefac04c4d09214257f885
npelletier2 <npelletier@wpi.edu>
2020-05-09 16:40:39 -0400

    Elbonian cipher algorithm done

--- src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
+++ src/main/java/edu/wpi/cs3733/entity/ElbonianCipher.java
@@ -4,13 +4,49 @@ package edu.wpi.cs3733.entity;
  * Elbonian cipher; you must implement the cipher and the observer pattern
  */
 public class ElbonianCipher implements Observer {
+	private String text;
+	private char[] lowerLetters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	private char[] upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
 	public void setText(String text){
+		int max = 140;
+		if(text.length() < 140) max = text.length();
+		text = text.substring(0, max);
 
+		String cipherText = "";
+		for(char c : text.toCharArray()){
+			if(new String(lowerLetters).contains(String.valueOf(c))){
+				cipherText += String.format("%02d", charArrayIndexOf(c, lowerLetters) + 1);
+			}else if(new String(upperLetters).contains(String.valueOf(c))){
+				cipherText += String.format("%02d", charArrayIndexOf(c, upperLetters) + 1);
+			}else if("123456789".contains(String.valueOf(c))){
+				cipherText += lowerLetters[Character.getNumericValue(c) - 1];
+			}else if(c == '0'){
+				cipherText += '_';
+			}else if(c == ' '){
+				cipherText += 'S';
+			}else if (",.!? ".contains(String.valueOf(c))){
+				cipherText += c;
+			}else{
+				this.text = "";
+				return;
+			}
+		}
+
+		this.text = cipherText;
 	}
 
 	public String getText(){
-		return null;
+		return text;
+	}
+
+	private static int charArrayIndexOf(char c, char[] arr){
+		for(int i = 0; i < arr.length; i++){
+			if(c == arr[i]){
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	@Override
--- src/test/java/CipherTests.java
+++ src/test/java/CipherTests.java
@@ -1,5 +1,8 @@
 import edu.wpi.cs3733.entity.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -9,18 +12,55 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CipherTests {
 	@Test
 	public void caesarCipherTest(){
-		CaesarCipher caesar = new CaesarCipher();
+		CaesarCipher cipher = new CaesarCipher();
 
-		caesar.setText("This is a boring message!?.,");
-		assertEquals("Gsrh rh z ylirmt nvhhztv!?.,", caesar.getText());
+		cipher.setText("This is a boring message!?.,");
+		assertEquals("Gsrh rh z ylirmt nvhhztv!?.,", cipher.getText());
 
-		caesar.setText("Ex@mple");
-		assertEquals("", caesar.getText());
+		cipher.setText("Ex@mple");
+		assertEquals("", cipher.getText());
 
-		caesar.setText("abcdefghijklmnopqrstuvwxyz .!,? ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		assertEquals("zyxwvutsrqponmlkjihgfedcba .!,? ZYXWVUTSRQPONMLKJIHGFEDCBA", caesar.getText());
+		cipher.setText("abcdefghijklmnopqrstuvwxyz .!,? ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		assertEquals("zyxwvutsrqponmlkjihgfedcba .!,? ZYXWVUTSRQPONMLKJIHGFEDCBA", cipher.getText());
+
+		char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.?! ".toCharArray();
+
+		Random rand = new Random();
+		String text = "";
+		for(int i = 0; i < 141; i++){
+			text += chars[Math.abs(rand.nextInt() % chars.length)];
+		}
+
+		cipher.setText(text);
+		assertEquals("", cipher.getText());
 	}
 
+	@Test
+	public void elbonianCipherTest(){
+		ElbonianCipher cipher = new ElbonianCipher();
 
+		cipher.setText("This is 1 message!");
+		assertEquals("20080919S0919SaS13051919010705!", cipher.getText());
+
+		cipher.setText("Ex@mple");
+		assertEquals("", cipher.getText());
+
+		cipher.setText("abcdefghijklmnopqrstuvwxyz .!,? ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		assertEquals("0102030405060708091011121314151617181920212223242526S.!,?S0102030405060708091011121314151617181920212223242526", cipher.getText());
+
+		char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.?! 0123456789".toCharArray();
+
+		Random rand = new Random();
+		String text = "";
+		for(int i = 0; i < 150; i++){
+			text += chars[Math.abs(rand.nextInt() % chars.length)];
+		}
+
+		ElbonianCipher cipher2 = new ElbonianCipher();
+
+		cipher.setText(text);
+		cipher2.setText(text.substring(0, 140));
+		assertEquals(cipher2.getText(), cipher.getText());
+	}
 
 }
